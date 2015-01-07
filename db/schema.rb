d5e777f09dedf7a36cfe102e885b01873ab09da9
Zoe <doticus@gmail.com>
2015-01-07 16:13:31 -0500

    Made categories not null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107200729) do
+ActiveRecord::Schema.define(version: 20150107210948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20150107200729) do
     t.string   "url",         null: false
     t.string   "description", null: false
     t.integer  "user_id"
-    t.integer  "category_id"
+    t.integer  "category_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
