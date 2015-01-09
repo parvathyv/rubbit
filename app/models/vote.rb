e22f73f1624cc9c0f4a590f6acc6fbb61f648eb1
Zoe <doticus@gmail.com>
2015-01-09 12:59:51 -0500

    Better validation for votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :article
   belongs_to :user
 
-  validates_uniqueness_of :user_id, scope: [:article_id, :vote_tally]
+  validates_uniqueness_of :user, scope: [:article, :vote_tally]
 end
